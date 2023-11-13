8ea9a368408b2ca276e59f81ced096780501007b
Carlos Silva <carlos0670@gmail.com>
2023-11-13 20:40:53 +0000

    project that fetches images through an initial api with third-party tenplate

--- Image_search_app/script.js
+++ Image_search_app/script.js
@@ -1,11 +1,12 @@
 const apiKey = 'I3bRpuwSVRnfVS9o7RbsdI2xsIPA4dgQestRnN4oo5rDz1y2LPH7VH5x';
 const endpoint = 'https://api.pexels.com/v1/curated';
+const searchEndpoint = 'https://api.pexels.com/v1/search';
 
-let dados; 
+let dados;
 
-async function fetchData() {
+async function fetchData(url) {
   try {
-    const response = await fetch(endpoint, {
+    const response = await fetch(url, {
       headers: {
         Authorization: apiKey,
       },
@@ -13,38 +14,59 @@ async function fetchData() {
 
     dados = await response.json();
 
-    // Você pode retornar os dados se necessário
     return dados;
   } catch (error) {
     console.error('Erro ao fazer a requisição:', error);
   }
 }
 
-async function usarDadosArmazenados() {
-  await fetchData();
+async function searchImages(query) {
+  const url = `${searchEndpoint}?query=${encodeURIComponent(query)}`;
+
+  return fetchData(url);
+}
+
+async function usarDadosArmazenados(data) {
   const gallery = document.getElementById('teste');
   gallery.innerHTML = '';
 
-  if (dados) {
-    console.log(dados.photos);
-    dados.photos.forEach(itens =>
+  if (data) {
+    data.photos.forEach(item =>
       gallery.innerHTML += `
-        <a class="gallery-item" href="${itens.url}">
-					<img src="${itens.src.medium}" alt="${itens.alt}">
-					<span class="overlay">
-						<h2>${itens.photographer}</h2>
-					</span>
-				</a>
-
-
+        <a class="gallery-item" href="${item.url}">
+          <img src="${item.src.medium}" alt="${item.alt}">
+          <span class="overlay">
+            <h2>${item.photographer}</h2>
+          </span>
+        </a>
       `
-     ); 
-  
+    );
   } else {
-
     console.log('Dados não disponíveis. Chame fetchData primeiro.');
   }
 }
 
-usarDadosArmazenados()
+document.addEventListener('DOMContentLoaded', function () {
+  const searchForm = document.getElementById('searchForm');
+  const searchInput = document.getElementById('searchInput');
 
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const searchTerm = searchInput.value.trim();
+
+    if (searchTerm !== '') {
+      searchImages(searchTerm)
+        .then(data => {
+          usarDadosArmazenados(data);
+        })
+        .catch(error => {
+          console.error('Erro ao pesquisar imagens:', error);
+        });
+    }
+  });
+
+  fetchData(endpoint).then(data => {
+    usarDadosArmazenados(data);
+  });
+});
